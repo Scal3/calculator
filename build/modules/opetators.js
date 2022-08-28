30cd7ae65c4238b7a74522d93cd186ef229520d1
scal3 <cool.goga501@gmail.com>
2022-08-28 17:04:50 +0300

    upd/convertValueWithOperator

--- build/modules/opetators.js
+++ build/modules/opetators.js
@@ -2,27 +2,20 @@ import { Operators, Symbols } from "../constants.js";
 import { replaceLastSimbolInStringWithOperator } from "../utils.js";
 const convertValueWithOperator = (calcValue, operator) => {
     const lastSymbol = calcValue[calcValue.length - 1];
-    if (lastSymbol === Operators.DIVIDE) {
-        return replaceLastSimbolInStringWithOperator(calcValue, operator);
+    switch (lastSymbol) {
+        case Operators.DIVIDE:
+            return replaceLastSimbolInStringWithOperator(calcValue, operator);
+        case Operators.MINUS:
+            return replaceLastSimbolInStringWithOperator(calcValue, operator);
+        case Operators.MULTIPLY:
+            return replaceLastSimbolInStringWithOperator(calcValue, operator);
+        case Operators.PLUS:
+            return replaceLastSimbolInStringWithOperator(calcValue, operator);
+        case Symbols.DOT:
+            return replaceLastSimbolInStringWithOperator(calcValue, operator);
+        default:
+            calcValue = `${calcValue}${operator}`;
+            return calcValue;
     }
-    if (lastSymbol === Operators.MINUS) {
-        return replaceLastSimbolInStringWithOperator(calcValue, operator);
-    }
-    if (lastSymbol === Operators.MULTIPLY) {
-        return replaceLastSimbolInStringWithOperator(calcValue, operator);
-    }
-    if (lastSymbol === Operators.PLUS) {
-        return replaceLastSimbolInStringWithOperator(calcValue, operator);
-    }
-    if (lastSymbol === Symbols.DOT) {
-        // add handler for 0.23123
-        const newVal = calcValue.split('');
-        newVal.pop();
-        const res = `${newVal.join('')}${operator}`;
-        return res;
-    }
-    // add 0/0 handler
-    calcValue = `${calcValue}${operator}`;
-    return calcValue;
 };
 export { convertValueWithOperator, };
