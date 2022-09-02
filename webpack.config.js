d5a662492a5f8d55f1179aec14629ce53035540d
scal3 <cool.goga501@gmail.com>
2022-09-02 17:32:20 +0300

    feat/create html support

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -26,4 +28,10 @@ module.exports = {
     port: 8080,
     open: true
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+  ]
 };
